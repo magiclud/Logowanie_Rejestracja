42ab3e1e00d33f050de43be65057d2f283db11e2
magiclud <you@example.com>
2014-05-07 12:55:29 +0200

    wchodzi do pobierzdaneodklienta

---
+++ src/pl/logowanie/net/PoierzDaneOdKlienta.java
@@ -0,0 +1,43 @@
+package pl.logowanie.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/PoierzDaneOdKlienta")
+public class PoierzDaneOdKlienta extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public PoierzDaneOdKlienta() {
+		super();
+	}
+
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("jestem w sevice method w pobieranie od klienta  ");
+		InputStream in = request.getInputStream();
+		BufferedReader r = new BufferedReader(new InputStreamReader(in));
+		StringBuffer buf = new StringBuffer();
+		String line;
+		while ((line = r.readLine()) != null) {
+			buf.append(line);
+		}
+		String fileName = "D:\\Programy\\eclipseEE\\wokspace\\Logowanie\\pobraneOdPlayera\\dane.txt";
+		String s = buf.toString();
+		System.out.println("Dane od klienta pobrane " + s);
+
+		String message = "pobrano od klienta jakies dane.";
+
+		request.setAttribute("wynikLogowania", message);
+		getServletContext().getRequestDispatcher("/index.jsp").forward(request,
+				response);
+	}
+
+}
