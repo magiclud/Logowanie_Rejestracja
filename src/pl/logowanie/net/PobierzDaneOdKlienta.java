ce60b73abf6b764bcb6c4a33bd933088ccd42ce6
magiclud <you@example.com>
2014-05-08 11:14:06 +0200

    wysylanie pliku

--- src/pl/logowanie/net/PobierzDaneOdKlienta.java
+++ src/pl/logowanie/net/PobierzDaneOdKlienta.java
@@ -1,12 +1,13 @@
 package pl.logowanie.net;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,19 +22,24 @@ public class PobierzDaneOdKlienta extends HttpServlet {
 	protected void service(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("jestem w sevice method w pobieranie od klienta  ");
-		InputStream in = request.getInputStream();
-		BufferedReader r = new BufferedReader(new InputStreamReader(in));
-		StringBuffer buf = new StringBuffer();
-		String line;
-		while ((line = r.readLine()) != null) {
-			System.out.println("line " + line);
-			buf.append(line);
-			System.out.println("buf " + buf.toString());
-		}
-		String fileName = "D:\\Programy\\eclipseEE\\wokspace\\Logowanie\\pobraneOdPlayera\\dane.txt";
-		String s = buf.toString();
-		System.out.println("Dane od klienta pobrane " + s);
 
+		int bytesRead;
+		InputStream in = request.getInputStream();
+		DataInputStream clientData = new DataInputStream(in);
+		String fileName = clientData.readUTF();
+		String newPath = "D:\\Programy\\eclipseEE\\wokspace\\Logowanie\\pobraneOdPlayera\\"
+				+ fileName;
+		OutputStream output = new FileOutputStream(newPath);
+		long size = clientData.readLong();
+		byte[] buffer = new byte[1024];
+		while (size > 0
+				&& (bytesRead = clientData.read(buffer, 0,
+						(int) Math.min(buffer.length, size))) != -1) {
+			output.write(buffer, 0, bytesRead);
+			size -= bytesRead;
+		}
+		output.close();
+		//in.close();
 		String message = "pobrano od klienta jakies dane.";
 
 		request.setAttribute("wynikLogowania", message);
